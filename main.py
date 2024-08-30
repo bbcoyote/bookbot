8f23c662a0be5c87f0941f3d4619bb0e64ad9cd9
Charles Huffman <huffmancharles14@gmail.com>
2024-08-30 17:16:27 -0500

    finished get_character_count function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     print(text)
     get_word_count(text)
+    get_character_count(text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,14 +16,11 @@ def get_word_count(string):
 def get_character_count(document):
     lower_case_doc = document.lower()
     char_dict = {}
-    value = 0
     for character in lower_case_doc:
-        # check if character already exists in dictionary
-        # if not add new key with a value of one.
-        # if it does exist in the dictionary increment the value.
-        # we can use the setdefault dict method to check if a key exists and add to the dictionary if it doesn't
-        character_count = char_dict.setdefault(character, value += 1)
+        char_dict[character] = char_dict.setdefault(character, 0) + 1
+    print(char_dict)
 
 
 
-main()
+# main()
+get_character_count("Hello this works like this")
