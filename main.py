461f62a809c7006d8c056ca259986f9d1e274ae0
Charles Huffman <huffmancharles14@gmail.com>
2024-08-29 20:59:58 -0500

    working on a function that gets the count for each character in a given string.

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ def get_word_count(string):
     words = string.split()
     print(len(words))
 
+def get_character_count(document):
+    lower_case_doc = document.lower()
+    char_dict = {}
+    value = 0
+    for character in lower_case_doc:
+        # check if character already exists in dictionary
+        # if not add new key with a value of one.
+        # if it does exist in the dictionary increment the value.
+        # we can use the setdefault dict method to check if a key exists and add to the dictionary if it doesn't
+        character_count = char_dict.setdefault(character, value += 1)
+
 
 
 main()
